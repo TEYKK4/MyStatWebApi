c20ed521016823d8367c3eb9c3d01ea8860cd119
TEYKK4 <teykasoc@gmail.com>
2022-12-10 00:13:30 +0400

    a

--- MyStatWebApi/Program.cs
+++ MyStatWebApi/Program.cs
@@ -34,15 +34,78 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapGet("api/v1/user/register/{name}/{surname}/{login}/{password}", async (IUserManager userManager, ISha256Encoder encoder, string name, string surname, string login, string password) => 
-    await userManager.AddAsync(new User
-{
-    Name = name,
-    Surname = surname,
-    Login = login,
-    Password = encoder.ComputeSha256Hash(password)
-}));
+app.MapPost("api/v1/user/register/{name}/{surname}/{login}/{password}", async (IUserManager userManager, ISha256Encoder encoder, string name, string surname, string login, string password) =>
+    {
+        if (await userManager.AddAsync(new User
+            {
+                Name = name,
+                Surname = surname,
+                Login = login,
+                Password = encoder.ComputeSha256Hash(password)
+            }))
+        {
+            return Results.Ok();
+        }
+        return Results.BadRequest();
+    });
 
-app.MapGet("api/v1/user/login/{login}/{password}", async (IUserManager userManager, ISha256Encoder encoder, string login, string password) => await userManager.GetByLoginPasswordAsync(login, encoder.ComputeSha256Hash(password)));
+app.MapPost("api/v1/user/remove/{id:int}", async (IUserManager userManager, int id) =>
+{
+    if (await userManager.RemoveAsync(id))
+    {
+        return Results.Ok();
+    }
+    return Results.BadRequest();
+});
+
+app.MapGet("api/v1/user/get/{id:int}", async (IUserManager userManager, int id) =>
+{
+    if (await userManager.GetByIdAsync(id) is { } user)
+    {
+        return Results.Json(user);
+    }
+    return Results.BadRequest();
+});
+
+app.MapGet("api/v1/user/login/{login}/{password}", async (IUserManager userManager, ISha256Encoder encoder, string login, string password) =>
+{
+    if (await userManager.GetByLoginPasswordAsync(login, encoder.ComputeSha256Hash(password)) is { } user)
+    {
+        return Results.Json(user);
+    }
+    return Results.BadRequest();
+});
+
+app.MapPost("api/v1/homework/add/{userId:int}/{description}", async (IHomeworkManager homeworkManager, int userId, string description) =>
+{
+    if (await homeworkManager.AddAsync(new Homework
+        {
+            Description = description,
+            UserId = userId,
+            DateTime = DateTime.Now
+        }))
+    {
+        return Results.Ok();
+    }
+    return Results.BadRequest();
+});
+
+app.MapPost("api/v1/homework/remove/{id:int}", async (IHomeworkManager homeworkManager, int id) =>
+{
+    if (await homeworkManager.RemoveAsync(id))
+    {
+        return Results.Ok();
+    }
+    return Results.BadRequest();
+});
+
+app.MapGet("api/v1/homework/get/{id:int}", async (IHomeworkManager homeworkManager, int id) =>
+{
+    if (await homeworkManager.GetByIdAsync(id) is { } homework)
+    {
+        return Results.Json(homework);
+    }
+    return Results.BadRequest();
+});
 
 app.Run();
